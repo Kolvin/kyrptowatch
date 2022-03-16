15a3f9d920fabf8b5598622b4ae184df68423bce
kolvin <15124052+Kolvin@users.noreply.github.com>
2022-03-16 22:02:12 +0000

    feat(live refresh): created custom hook for useInterval - https://overreacted.io/making-setinterval-declarative-with-react-hooks/
    Signed-off-by: kolvin <15124052+Kolvin@users.noreply.github.com>

---
+++ app/src/hooks/useInterval.js
@@ -0,0 +1,23 @@
+import {useEffect, useRef } from 'react';
+
+export const useInterval = (callback, delay) => {
+  let savedCallback = useRef();
+
+  // Remember the latest callback.
+  useEffect(() => {
+    savedCallback.current = callback;
+  }, [callback]);
+
+  // Set up the interval.
+  useEffect(() => {
+    function tick() {
+      savedCallback.current();
+    }
+    if (delay !== null) {
+      let id = setInterval(tick, delay);
+      return () => clearInterval(id);
+    }
+  }, [delay]);
+
+  // return { word, end, appendLetter };
+};
